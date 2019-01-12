876de99e60bdd475e94e5f628b214c4e0a8b378f
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-01-11 22:49:11 -0600

    Friday

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,107 +6,75 @@ var config = {
     projectId: "ashleys-train-scheduler",
     storageBucket: "ashleys-train-scheduler.appspot.com",
     messagingSenderId: "579029626638"
-};
+  };
 
 firebase.initializeApp(config);
 
 // Create a variable to reference the database.
 var database = firebase.database();
 
-// Create other variables to reference in the database
-var trainName = "";
-var destination = "";
-var firstTrain = "";
-var minutes = 0;
-
-// CONNECTIONS ------------------------------------------------------------------------------------------
-// connectionsRef references a specific location in our database.
-// All of our connections will be stored in this directory.
-var connectionsRef = database.ref("/connections");
-
-// '.info/connected' is a special location provided by Firebase that is updated every time
-// the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-var connectedRef = database.ref(".info/connected");
-
-// When the client's connection state changes...
-connectedRef.on("value", function (snap) {
-
-    // If they are connected..
-    if (snap.val()) {
-
-        // Add user to the connections list.
-        var con = connectionsRef.push(true);
-
-        // Remove user from the connection list when they disconnect.
-        con.onDisconnect().remove();
-    }
-});
-
-// When first loaded or when the connections list changes...
-connectionsRef.on("value", function (snapshot) {
-
-    // Display the viewer count in the html.
-    // The number of online users is the number of children in the connections list.
-    $("#watchers").text(snapshot.numChildren());
-});
-// END CONNECTIONS------------------------------------------------------------------------------------
-
-// On click function for submit button
-$("#add-train").on("click", function (event) {
+// On click function for submit button to add train info
+$("#submit").on("click", function (event) {
 
     event.preventDefault();
 
     // STORING AND RETRIEVING INFO FROM MOST RECENT USER 
-    // Get the "values" from the form fields and store them in the variables
-    trainName = $("#train-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    firstTrain = parseInt($("#first-train-time-input").val().trim());
-    minutes = parseInt($("#minutes-input").val().trim());
+    // Grab the "values" from the form fields and store them in variables
+    var trainName = $("#train-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = parseInt($("#first-input").val().trim());
+    var minutes = parseInt($("#minutes-input").val().trim());
 
-    // PUSH THE INFO INTO THE CURRENT TRAIN SCHEDULE
-    // Create a new variable that will hold a "<p>" tag.
-    var trainSchedule = $("<tr>");
-
-    // Then give it to an ID - THIS ISN'T RIGHT
-    trainSchedule.attr("add-train", trainName, destination, firstTrain, minutes);
-    trainSchedule.text(trainName, destination, firstTrain, minutes);
-    $(".card-body").append(trainSchedule);
-  
-    // Sets the information into the Firebase database (reminder: must be inside click handler)
-    database.ref().set({
+    // Create a variable to consolidate the input data
+    var newTrain = {
         trainName: trainName,
         destination: destination,
         firstTrain: firstTrain,
         minutes: minutes,
-    });
+    }
+    
+    // Push the input data into the Firebase database (reminder: must be inside click handler)
+    database.ref().push(newTrain);
+
+    // Check information in console
+    console.log(newTrain.trainName);
+    console.log(newTrain.destination);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.minutes);
+
+    // Clear input area
+    $("#train-input").val("");
+    $("#destination-input").val("");
+    $("#first-input").val("");
+    $("#minutes-input").val("");
 
 });
 
-// Need to push the information entered into the form into the database
+// Push input data into the current train schedule, including adding a row each time
+database.ref().on("child_added", function (childSnapshot) {
 
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().trainName;
+    var destination = childSnapshot.val().destination;
+    var firstTrain = childSnapshot.val().firstTrain;
+    var minutes = childSnapshot.val().minutes;
 
+    // Calculate the time until the newly input train arrives
+    //var timeCalc = moment().diff(moment(firstTrain, "X"), "minutes");
+    //console.log(minutes);
 
-// How to call information stored in the database, AKA Firebase watcher & initial loader (must be in global scope)
-// “On” when a value in the database changes, call the function:
-// “Snapshot” is the view of what the database looks like when the function is called
-// Console.log says give me the info inside the database
-// Click count says go in the database, find something called click count, load it
+    // Create the new row and append new data into the train schedule
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(firstTrain),
+        $("<td>").text(minutes),
+    );
 
-database.ref().on("value", function (snapshot) {
-    var value = snapshot.val();
-    console.log(value)
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
 
-
-    // Taken from example - need to see how it works with this one
-    // clickCounter = snapshot.val().clickCount;
-
-    // error handler
-    function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    };
-
-});
-
+})
 
 
